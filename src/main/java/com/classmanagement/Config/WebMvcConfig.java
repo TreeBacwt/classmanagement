bab87a0cfaca3f61c0e58914ee77d494e9a40a91
WaterGod <736103253@qq.com>
2023-04-08 14:36:36 +0800

    允许跨域访问

---
+++ src/main/java/com/classmanagement/Config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.classmanagement.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.FormContentFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("Content-Type","X-Requested-With","accept,Origin","Access-Control-Request-Method","Access-Control-Request-Headers","token")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true);
+    }
+
+    @Bean
+    public FormContentFilter httpPutFormContentFilter(){
+        return new FormContentFilter();
+    }
+}
