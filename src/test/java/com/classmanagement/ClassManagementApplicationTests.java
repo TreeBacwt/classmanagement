b25f67e5c05e1283be65597ba2ccf1bb6203ddde
WaterGod <736103253@qq.com>
2023-04-18 12:25:22 +0800

    测试

--- src/test/java/com/classmanagement/ClassManagementApplicationTests.java
+++ src/test/java/com/classmanagement/ClassManagementApplicationTests.java
@@ -1,5 +1,6 @@
 package com.classmanagement;
 
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,18 +9,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.DigestUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 @SpringBootTest
 class ClassManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	void test(){
-		Logger logger = LoggerFactory.getLogger("what");
-		logger.info("111");
-	}
+    @Test
+    void test() {
+//		Logger logger = LoggerFactory.getLogger("what");
+//		logger.info("111");
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("语文", 11);
+        map.put("数学", 23);
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(map));
+        System.out.println("语文");
+    }
 
 }
